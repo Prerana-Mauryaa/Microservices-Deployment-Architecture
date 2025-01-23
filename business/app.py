bcbc873f974f86ba8bf8182995fd7b345b3d4744
Prerana Maurya <preranamaurya012389@gmail.com>
2025-01-23 13:46:33 +0530

    Update app.py

--- business/app.py
+++ business/app.py
@@ -4,7 +4,7 @@ import demo_pb2
 import demo_pb2_grpc
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/business/static')
 
 @app.route('/business', methods=['POST', 'GET'])
 def submit_form():
